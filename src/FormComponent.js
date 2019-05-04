d9364664e833b5df1d89bcaee7a7789a1390bd15
TomaszDudekPL <tdkontakt@gamil.com>
2019-05-05 01:25:33 +0200

    Introduced search for second word, jumbotron, new css, informing about use cases

--- src/FormComponent.js
+++ src/FormComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Col, Container, Form, FormGroup, Input, Label, Row} from 'reactstrap';
+import {Button, Col, Container, Form, FormGroup, Input, Jumbotron, Row} from 'reactstrap';
 import SearchResultItems from "./SearchResultItems";
 import * as firebase from "firebase/app";
 import "firebase/database";
@@ -26,7 +26,9 @@ export default class FormComponent extends React.Component {
     base: null,
     showWholeBase: false,
     name: '',
-    numberOfAllUseCases: 0
+    numberOfAllUseCases: 0,
+    wantedWords: [],
+    ucInfoObj: null
   };
 
   changeBaseToProperForm(val) {
@@ -61,120 +63,139 @@ export default class FormComponent extends React.Component {
 
   filterList = (event) => {
 
-    if (!this.state.items.length) {
+    let updatedList = [];
+    let base = this.state.base;
+    let ucArr = new Set();
+    let wantedValue;
+
+    let removeSpacesFunc = (word) => word ? word.replace(/^\s+|\s+$/g, "").replace(/\s+/g, " ") : null;
+    let getLowerCaseFunc = (character) => character.toLowerCase();
+
+    let arrOfKeyWords = event.target.value.split(' ');
+    arrOfKeyWords = arrOfKeyWords.filter(function (el) {
+      return el !== null && el !== "";
+    });
+
+
+    console.log('a1');
+    console.log('!arrOfKeyWords.length: ', !arrOfKeyWords.length);
+
+
+    if (!this.state.items.length || arrOfKeyWords.length === 1) {
+
+      console.log('pierwszy przypadek');
 
       this.setState({
         name: event.value,
         showWholeBase: false
       });
 
-      if (event.target.value.length > 3) {
+      console.log('event.target.value.length: ', event.target.value.length);
 
-        let base = this.state.base;
-        let updatedList = [];
-        let ucArr = new Set();
+      if (event.target.value.length >= 3) {
+
+        updatedList = [];
+
+        console.log('event.target.value.length------- wszedłem tu');
 
         base.forEach(arrOfUC => {
 
-          arrOfUC[1].forEach(
-            function (uc) {
+          arrOfUC[1].forEach(uc => {
 
-              if (uc.toLowerCase().search(
-                event.target.value.toLowerCase()
-                  .replace(/^\s+|\s+$/g, "")
-                  .replace(/\s+/g, " ")) !== -1) {
-                ucArr.add(uc);
-              }
+            uc = getLowerCaseFunc(uc);
+            wantedValue = getLowerCaseFunc(event.target.value);
+            wantedValue = removeSpacesFunc(wantedValue);
 
-            });
+            if (uc.search(wantedValue) !== -1) ucArr.add(uc);
 
-          if (ucArr.size) {
-            updatedList.push([arrOfUC[0], [...ucArr]]);
-          }
+          });
+
+          if (ucArr.size) updatedList.push([arrOfUC[0], [...ucArr]]);
+
           ucArr = new Set();
 
         });
 
+        // ready results to be rendered
         this.setState({
-          items: updatedList
+          items: updatedList,
+          wantedWords: [wantedValue]
         });
 
-      } else {
-        this.setState({
-          items: []
-        });
       }
 
-      // Experimental searching. It still does not work correctly.  Lines: 121 to 191
+      // Experimental searching. It still does not work correctly.  Lines: 121 to 199
 
     } else if (/\s+/.test(event.target.value)) {
+      console.log('drugi przypadek');
+      console.log('arrOfKeyWords: ', arrOfKeyWords);
 
-      let arrOfKeyWords = event.target.value.split(' ');
-      arrOfKeyWords = arrOfKeyWords.filter(function (el) {
-        return el !== null && el !== "";
-      });
+      let firstKeyWord = removeSpacesFunc(arrOfKeyWords[0]);
+      let secondKeyWord = removeSpacesFunc(arrOfKeyWords[1]);
+      let thirdKeyWord = removeSpacesFunc(arrOfKeyWords[2]);
 
-      if (arrOfKeyWords.length > 1) {
+      let wantedWords = [];
+
+      wantedWords.push(firstKeyWord);
+      if (secondKeyWord.length > 2) wantedWords.push(secondKeyWord);
+      if (secondKeyWord.length > 2) wantedWords.push(secondKeyWord);
+
+      console.log('firstKeyWord: ', firstKeyWord);
+      console.log('secondKeyWord: ', secondKeyWord);
+      console.log('thirdKeyWord: ', thirdKeyWord);
+
+      if (secondKeyWord || thirdKeyWord) {
 
         let base = this.state.base;
         let updatedList = [];
         let ucArr = new Set();
-
 
         base.forEach(arrOfUC => {
 
           arrOfUC[1].forEach(
             function (uc) {
 
-              if (uc.toLowerCase().search(
-                arrOfKeyWords[0].toLowerCase()
-                  .replace(/^\s+|\s+$/g, "")
-                  .replace(/\s+/g, " ")) !== -1) {
-                ucArr.add(uc);
-              }
-
-              if (arrOfKeyWords[1]) {
-
-                if (uc.toLowerCase().search(
-                  arrOfKeyWords[1].toLowerCase()
-                    .replace(/^\s+|\s+$/g, "")
-                    .replace(/\s+/g, " ")) !== -1) {
+              if (uc.search(firstKeyWord) !== -1) {
+                if (!arrOfKeyWords[1]) {
                   ucArr.add(uc);
                 }
-              }
-
-              if (arrOfKeyWords[2]) {
-                if (uc.toLowerCase().search(
-                  arrOfKeyWords[2].toLowerCase()
-                    .replace(/^\s+|\s+$/g, "")
-                    .replace(/\s+/g, " ")) !== -1) {
-                  ucArr.add(uc);
+                if (arrOfKeyWords[1]) {
+                  if (uc.search(secondKeyWord) !== -1) {
+                    if (!arrOfKeyWords[2]) {
+                      ucArr.add(uc);
+                    }
+                  }
+                  if (arrOfKeyWords[2]) {
+                    if (uc.search(thirdKeyWord) !== -1) {
+                      ucArr.add(uc);
+                    }
+                  }
                 }
               }
-
             });
 
           if (ucArr.size) {
             updatedList.push([arrOfUC[0], [...ucArr]]);
+            ucArr = new Set();
+            this.setState({
+              items: updatedList,
+              wantedWords: wantedWords
+            });
           }
-          ucArr = new Set();
 
         });
 
-        this.setState({
-          items: updatedList
-        });
 
-      } else {
-
-        // this.setState({
-        //   items: []
-        // });
-
-        // this.filterList();
       }
 
     }
+
+    if (event.target.value.length < 4) {
+      this.setState({
+        items: []
+      })
+    }
+    console.log('a3');
   };
 
   preventActionHandler = (event) => {
@@ -191,14 +212,30 @@ export default class FormComponent extends React.Component {
     )
   }
 
+  onItemClicked = (ucInfoObj) => {
+    this.setState({
+      ucInfoObj: ucInfoObj
+    })
+  };
+
 
   render() {
     return (
       <div className="main-label">
-        <Label for="search" className="form-mainLabel_mod">USE CASES SEARCH</Label>
-        <Form>
-          <FormGroup>
-            <Container>
+        <Container fluid>
+          <Row>
+            <Col sm="12" md={{size: 12, offset: 0}}>
+              <Jumbotron fluid className="jumbotron_mod">
+                <h1 className="display-7 form-mainLabel_mod">USE CASES SEARCH</h1>
+                <p className="lead">
+                  <div id="hideMessage">{this.state.ucInfoObj? this.state.ucInfoObj.uc: ''}</div>
+                </p>
+              </Jumbotron>
+            </Col>
+          </Row>
+
+          <Form>
+            <FormGroup>
               <Row>
                 <Col sm="12" md={{size: 12}} className="form-input_mod">
                   <Input type="search" value={this.state.name} name="search" id="useCasesSearch" bsSize="lg"
@@ -206,25 +243,29 @@ export default class FormComponent extends React.Component {
                          onChange={this.filterList}
                          onKeyPress={this.preventActionHandler}/>
                 </Col>
+              </Row>
+              <Row>
                 {!this.state.showWholeBase ?
                   (<Col sm="12" md={{size: 4, offset: 4}} className="form-button_mod">
-                    <Button color="warning" onClick={this.showAllUseCases}>Show All ({this.state.numberOfAllUseCases})
+                    <Button block color="warning" onClick={this.showAllUseCases}>Show All
+                      ({this.state.numberOfAllUseCases})
                       Use Cases</Button>
                   </Col>) :
 
                   (<Col sm="12" md={{size: 4, offset: 4}} className="form-button_mod">
-                    <Button color="danger" onClick={this.hideAllUseCases}>Hide All Use Cases</Button>
+                    <Button block color="danger" onClick={this.hideAllUseCases}>Hide All Use Cases</Button>
                   </Col>)
                 }
               </Row>
               <Row>
                 <Col sm="12" md={{size: 12, offset: 0}}>
-                  <SearchResultItems items={this.state.items}>.</SearchResultItems>
+                  <SearchResultItems items={this.state.items} wantedWords={this.state.wantedWords}
+                                     itemClicked={this.onItemClicked}>.</SearchResultItems>
                 </Col>
               </Row>
-            </Container>
-          </FormGroup>
-        </Form>
+            </FormGroup>
+          </Form>
+        </Container>
       </div>
     );
   }
--- src/SearchResultItems.js
+++ src/SearchResultItems.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import Highlighter from "react-highlight-words";
 
 export default class SearchResultItems extends React.Component {
 
@@ -14,13 +15,21 @@ export default class SearchResultItems extends React.Component {
       this.props.items ? this.props.items.map(arr => {
         let arrWithData = arr[0].split('%5C');
         return arr[1].map(uc => {
+          uc = uc.charAt(0).toUpperCase() + uc.substring(1);
+          let ucInfoObj = {arr: arrWithData, uc};
             return (
-              <Breadcrumb key={uc + Math.floor(Math.random()*1000)} className="list-item_mod" onClick={this.toggle}>
+              <Breadcrumb key={uc + Math.floor(Math.random()*1000)} className="list-item_mod" onClick={this.props.itemClicked.bind(null, ucInfoObj)}>
                 <span className="item-number_mod">{(number++) - (numberState - 1)}.</span>
                 <BreadcrumbItem>{arrWithData[0]}</BreadcrumbItem>
                 <BreadcrumbItem>{arrWithData[1]}</BreadcrumbItem>
-                <BreadcrumbItem active>{arrWithData[2] + '.js'}</BreadcrumbItem>
-                <span className="list-text_mod">{uc}</span>
+                <BreadcrumbItem>{arrWithData[2] + '.js'}</BreadcrumbItem>
+                <Highlighter
+                  className="list-text_mod "
+                  highlightClassName="highlight-text"
+                  searchWords={this.props.wantedWords}
+                  autoEscape={true}
+                  textToHighlight={uc}
+                />
               </Breadcrumb>
             )
           }
