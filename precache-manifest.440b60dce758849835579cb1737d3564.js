2d56b1b2ac835e1fb6eb17d2e77964394bb744f4
TomaszDudekPL <tdkontakt@gamil.com>
2019-05-07 18:15:32 +0200

    Updates

---
+++ precache-manifest.440b60dce758849835579cb1737d3564.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e2288ccefc0c986636eeefd244c7470",
+    "url": "/use-cases-search/index.html"
+  },
+  {
+    "revision": "81ea7fd8d9d4e4fac83a",
+    "url": "/use-cases-search/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ab55682cdc5cfd9b66f2",
+    "url": "/use-cases-search/static/css/main.19366886.chunk.css"
+  },
+  {
+    "revision": "81ea7fd8d9d4e4fac83a",
+    "url": "/use-cases-search/static/js/2.b3822440.chunk.js"
+  },
+  {
+    "revision": "ab55682cdc5cfd9b66f2",
+    "url": "/use-cases-search/static/js/main.370dce11.chunk.js"
+  },
+  {
+    "revision": "74c86c658818f86fb1d7",
+    "url": "/use-cases-search/static/js/runtime~main.a12457b7.js"
+  }
+]);
