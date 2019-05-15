b04618df9d5002bf6d913a1ffe607c74636ba1a1
TomaszDudekPL <tdkontakt@gamil.com>
2019-05-15 19:28:29 +0200

    New styles for results and jumbotron. Add link to github for each file.

--- src/BreadcrumbItems.js
+++ src/BreadcrumbItems.js
@@ -10,7 +10,11 @@ export default class BreadcrumbItems extends React.Component {
     return (
       arrWithData ? arrWithData.map((data, index) => {
           return (
-            (index === arrWithData.length -1 ) ? <BreadcrumbItem active key={data}>{data + '.js'}</BreadcrumbItem>: <BreadcrumbItem key={data}>{data}</BreadcrumbItem>
+            (index === arrWithData.length - 1) ?
+
+              <BreadcrumbItem active key={data} className="breadcrumb-link_mod">
+                <a target="_blank" rel="noopener noreferrer"  href={"https://github.com/sgrouples/Frontend-E2E-Tests/blob/master/test/specs/" + arrWithData.join('/') + '.js'}>{data + '.js'}</a>
+              </BreadcrumbItem>: <BreadcrumbItem key={data} className="breadcrumb_mod">{data}</BreadcrumbItem>
           )
         }
       ) : null)
