9fa45b5d16783376050ed5a6ff013bc8b49cd31e
TomaszDudekPL <tdkontakt@gamil.com>
2020-01-23 21:57:04 +0100

    Steps of Use Cases (WIP)

--- src/helpers/changeBaseEngine.js
+++ src/helpers/changeBaseEngine.js
@@ -22,29 +22,41 @@ export default function (val) {
 
   entries.forEach(function (arr) {
 
-    arr[1] = Object.keys(arr[1]);
-    number += arr[1].length;
+    const filePath = arr[0]; // string: path to file: "CONSUMER%2FGROUPS%2FGENERAL_GROUPS%2Fgroup_uc05"
+    const arrOfUseCasesKeys = Object.keys(arr[1]); // array: files paths: ["HSH_Settings; !guest; !members; Step 2of7: Members settings; Temporary deactivation of Guest account", (...)]
+    const arrOfUseCasesValues = Object.values(arr[1]); // array: values: [ "BEFORE: (...)", "STEP 1: (...)", "STEP 2: (...)", "ASSERT: (...)", "END: (...)" ]
 
-    if (/CONSUMER|LIVE/mi.test(arr[0])) {
+    arr[1] = Object.keys(arr[1]); // <----------- THIS IS CLUE
+
+    // Najpierw: każdy Use Case zamknąć w dodatkową tablicę, a na drugim miejscu umieścić tablicę ze Step-ami
+    // Następnie: naprawić to co się zepsuje przez powyższe w innych plikach
+
+    number += arrOfUseCasesKeys.length;
+
+    if (/CONSUMER|LIVE/mi.test(filePath)) {
 
       consumerList.push(arr);
-      numberConsumer += arr[1].length;
-      let allHashTags = returnAllHashTags(arr[1][0]);
+
+      numberConsumer += arrOfUseCasesKeys.length;
+      let allHashTags = returnAllHashTags(arrOfUseCasesKeys[0]);
       allHashTags.forEach(hashTag => {
         uniqHashTagsForConsumer.add(hashTag);
       });
 
-    } else if (/PRO/mi.test(arr[0])) {
+    } else if (/PRO/mi.test(filePath)) {
 
       proList.push(arr);
-      numberPro += arr[1].length;
-      let allHashTags = returnAllHashTags(arr[1][0]);
+
+      numberPro += arrOfUseCasesKeys.length;
+      let allHashTags = returnAllHashTags(arrOfUseCasesKeys[0]);
       allHashTags.forEach(hashTag => {
         uniqHashTagsForPro.add(hashTag);
       });
 
     }
   });
+
+  // console.log('typeof consumerList: ', typeof consumerList, JSON.stringify(consumerList, null, 4));
 
   return {
     base: entries,
