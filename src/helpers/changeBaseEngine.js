a12b2b0e20b21e817de518e8f1b8b315a4b4e920
TomaszDudekPL <tdkontakt@gamil.com>
2020-05-18 12:38:14 +0200

    Cleaning part 3.

--- src/helpers/changeBaseEngine.js
+++ src/helpers/changeBaseEngine.js
@@ -1,5 +1,5 @@
 export default function (val) {
-  let number = 0, numberConsumer = 0, numberPro = 0;
+  let numberConsumer = 0, numberPro = 0;
   let entries = Object.entries(val);
   let consumerList = [];
   let proList = [];
@@ -25,12 +25,7 @@ export default function (val) {
     const filePath = arr[0]; // string: path to file: "CONSUMER%2FGROUPS%2FGENERAL_GROUPS%2Fgroup_uc05"
     const arrOfUseCasesKeys = Object.keys(arr[1]); // array: files paths: ["HSH_Settings; !guest; !members; Step 2of7: Members settings; Temporary deactivation of Guest account", (...)]
 
-    // eslint-disable-next-line no-unused-vars
-    const arrOfUseCasesValues = Object.values(arr[1]); // array: values: [ "BEFORE: (...)", "STEP 1: (...)", "STEP 2: (...)", "ASSERT: (...)", "END: (...)" ]
-
     arr[1] = Object.entries(arr[1]);
-
-    number += arrOfUseCasesKeys.length;
 
     if (/CONSUMER|LIVE/mi.test(filePath)) {
 
@@ -61,7 +56,6 @@ export default function (val) {
     proList: proList,
     numberOfAllUCforConsumer: numberConsumer,
     numberOfAllUCforPro: numberPro,
-    numberOfAllUseCases: number,
     uniqHashTagsForConsumer: [...uniqHashTagsForConsumer],
     uniqHashTagsForPro: [...uniqHashTagsForPro]
   };
