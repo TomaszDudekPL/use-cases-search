7b6d44e32d4ca2facb31b3eb7b75046da0850ea8
TomaszDudekPL <tdkontakt@gamil.com>
2020-06-03 18:19:17 +0200

    Search input style.

--- src/Components/SearchInputComponent.js
+++ src/Components/SearchInputComponent.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import {Col, Input, Row} from "reactstrap";
+import {Button, Col, FormGroup, Input, InputGroup, InputGroupAddon, Row} from "reactstrap";
 import {preventActionHandler} from "../helpers/helperFunctions";
 
 const SearchInputComponent = ({multipleFuncOnChangeHandler, name}) => {
   return (
     <Row>
-      <Col sm="12" md={{size: 8, offset: 2}} className="form-input_mod">
-        <Input type="search" spellCheck="false" value={name} name="search" id="useCasesSearch"
-               bsSize="lg"
-               placeholder="Narrow down the results..."
-               onChange={multipleFuncOnChangeHandler}
-               onKeyPress={preventActionHandler}
-        />
+      <Col sm="12" md={{size: 8, offset: 2}}>
+
+        <FormGroup>
+
+          <InputGroup>
+            <Input spellCheck="false"
+                   type="text"
+                   value={name}
+                   name="search"
+                   id="useCasesSearch"
+                   bsSize="lg"
+                   valid
+                   placeholder="Type the word you are looking for and press ENTER"
+                   onChange={multipleFuncOnChangeHandler}
+                   onKeyPress={preventActionHandler}
+            />
+            <InputGroupAddon addonType="append"><Button outline color="danger">✕</Button></InputGroupAddon>
+          </InputGroup>
+
+        </FormGroup>
+
       </Col>
     </Row>
   );
